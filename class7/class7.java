69bd744dbee4dcde6565213cdbc28bccd745a5ab
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-07 01:09:05 +0530

    learn switch statement and break

---
+++ class7/class7.java
@@ -0,0 +1,41 @@
+package class7;
+
+public class class7 {
+    public static void main(String[] args) {
+        int n = 10;
+
+        // System.out.println("monday");
+        // System.out.println("tueday");
+        // System.out.println("wednesday");
+        // System.out.println("thursday");
+        // System.out.println("friday");
+        // System.out.println("sunday");
+
+        switch (n) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tueday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("monday");
+                break;
+            default:
+                System.out.println("sunday");
+                break;
+        }
+
+    }
+}
+
+// 2:17
