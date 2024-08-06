510365093851896787e3c94f729fbe61d302356f
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-07 00:59:12 +0530

    learn turnary oprator

---
+++ class6/class6.java
@@ -0,0 +1,20 @@
+package class6;
+
+public class class6 {
+ public static void main(String[] args) {
+    int n = 4;
+    int result = 0;
+//     if(n%2 == 0){
+//         result = 10;
+//     }
+//     else{
+//         result = 20;
+//     }
+// System.out.println(result);
+    result = n%2==0 ? 10 : result>0 ? 5:3;
+System.out.println(result);
+
+
+}
+
+}
