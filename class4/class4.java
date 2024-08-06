61b0ff7b0e488281ca62777a3edcfe7ab1c33d22
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-06 23:17:46 +0530

    learn the bitwise oprator

---
+++ class4/class4.java
@@ -0,0 +1,20 @@
+package class4;
+
+class class4 {
+   public static void main(String[] args){
+
+    // logical opratiors & - AND || |-or || !-not
+    double x = 8.9;
+    double y = 9.8;
+
+    int a = 8;
+    int b = 6;
+
+    boolean result = x<=y && a>b; 
+    boolean result2 = x<=y|| a>b; 
+    boolean result3 = x<=y && a>b; 
+    System.out.println(result);
+    System.out.println(result2);
+    System.out.println(result);
+   }  
+} 
