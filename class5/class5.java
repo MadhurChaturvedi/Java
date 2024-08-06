5ba0e772370b0e351e0a77ab49724b8de1bfd0dc
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-07 00:50:36 +0530

    Add the if-else with bit oprators

--- class5/class5.java
+++ class5/class5.java
@@ -1,15 +1,17 @@
 package class5;
-
 // conditional statements.
 public class class5 {
     public static void main(String[] args) {
-        int x= 18;
-        if(x > 10)
-            System.out.println("Hello");
-        
-        else{
-            System.out.println("Bye");
+        int x = 8;
+        int y = 17;
+        int z = 6;
+        if(x > y && x > z)
+            System.out.println(x);
+        else if(y>x && y>z){
+            System.out.println(y);
         }
-       
+        else {
+            System.out.println(z);
+        }
     }
 }
