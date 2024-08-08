c74a53aa59078343808e26b69f1c3e5405070395
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-08 23:40:12 +0530

    lern do while loop & for loop

---
+++ class9/class9.java
@@ -0,0 +1,20 @@
+package class9;
+
+public class class9 {
+
+    public static void main(String[] args) {
+        // int i = 1;
+        // do {
+        // System.out.println("Hi " + i);
+        // i++;
+        // } while (i < 4);
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Day" + i);
+            // for (int j = 1; j <= 9; j++) {
+            // System.out.println(" " + (j + 8) + " - " + (j + 9));
+            // }
+            
+        }
+    }
+}
