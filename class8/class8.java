09604c1a06d77b18db43dac39b99d3383d000ffa
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-08 11:16:20 +0530

    learn whiile and nested loops

---
+++ class8/class8.java
@@ -0,0 +1,17 @@
+package class8;
+
+public class class8 {
+    public static void main(String[] args) {
+        // loop - while , do-while , for nested while loop
+        int i = 1;
+        while (i <= 4) {
+            System.out.println("Hi " + i);
+            int j = 1;
+            while (j <= 3) {
+                System.out.println("Hello " + j);
+                j++;
+            }
+            i++;
+        }
+    }
+}
