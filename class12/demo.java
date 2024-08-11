04b70a0e44bb63eabf78a14ce88a4d3eebbfea9d
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-12 00:22:14 +0530

    learn 2d array and blank array and using wiht loop and how can we use rendom inbuilt funcuotn in java and also Enhanced for-loop also nested loop

--- class12/demo.java
+++ class12/demo.java
@@ -7,15 +7,39 @@ class demo {
         // System.out.println(num[0]);
 
         // int nums[] = { 1, 2, 3, 5 };
-        int nums[] = new int[4];
-        nums[0] = 4;
-        nums[1] = 5;
-        nums[2] = 6;
-        nums[3] = 7;
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+        // int nums[] = new int[4];
+        // nums[0] = 4;
+        // nums[1] = 5;
+        // nums[2] = 6;
+        // nums[3] = 7;
+        // for (int i = 0; i < nums.length; i++) {
+        // System.out.println(nums[i]);
+        // }
+
+        // multydimensinal Array
+        int arr[][] = new int[3][4];
+
+        // arr[1][1] = 5;
+        // System.out.println(arr[1][1]);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = (int) (Math.random() * 10);
+            }
         }
 
-        // System.out.println(nums[0]);
+        // for (int i = 0; i < 3; i++) {
+        // for (int j = 0; j < 4; j++) {
+        // System.out.print(arr[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        for (int n[] : arr) {
+            for (int m : n) {
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
