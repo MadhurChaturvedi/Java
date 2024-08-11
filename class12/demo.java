47da85473a08db1e4f90446ecba2f5fef5bea7ff
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-12 00:04:34 +0530

    learn empty array and using with loops

--- class12/demo.java
+++ class12/demo.java
@@ -2,8 +2,20 @@
 class demo {
     public static void main(String[] args) {
         // Array
-        int num[] = { 5, 4, 4 };
-        int num1[] = new int[4];
-        System.out.println(num[0]);
+        // int num[] = { 5, 4, 4 };
+        // int num1[] = new int[4];
+        // System.out.println(num[0]);
+
+        // int nums[] = { 1, 2, 3, 5 };
+        int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 5;
+        nums[2] = 6;
+        nums[3] = 7;
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+
+        // System.out.println(nums[0]);
     }
 }
