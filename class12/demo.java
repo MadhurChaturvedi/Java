6ae88f9656fce861bb8acb43c588852b7fe17f2a
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-11 23:56:24 +0530

    learn array

---
+++ class12/demo.java
@@ -0,0 +1,9 @@
+
+class demo {
+    public static void main(String[] args) {
+        // Array
+        int num[] = { 5, 4, 4 };
+        int num1[] = new int[4];
+        System.out.println(num[0]);
+    }
+}
