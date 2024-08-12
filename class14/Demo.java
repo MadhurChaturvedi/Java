1a918c8a576ebb6a567d92d2eec430d52f6c42b5
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-13 00:51:20 +0530

    string is a class

--- class14/Demo.java
+++ class14/Demo.java
@@ -3,9 +3,10 @@ package class14;
 public class Demo {
     public static void main(String[] args) {
         // String
-        String name = new String("Madhur");
+        // String name = new String("Madhur"); but we can do like this string is
+        // non-primitive is string class
+        String name = "Madhur";
         System.out.println(name);
-
 
     }
 }
