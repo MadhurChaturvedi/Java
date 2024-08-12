f00790806d5c03eddde2061067621b4924d21216
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-13 00:48:26 +0530

    learn string

---
+++ class14/Demo.java
@@ -0,0 +1,11 @@
+package class14;
+
+public class Demo {
+    public static void main(String[] args) {
+        // String
+        String name = new String("Madhur");
+        System.out.println(name);
+
+
+    }
+}
