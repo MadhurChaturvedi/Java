1604f2e636130013bbb29eab6faf3d448190af99
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-15 22:47:34 +0530

    Constructor

--- class17/demo.java
+++ class17/demo.java
@@ -1,11 +1,19 @@
 package class17;
 
+// Inheritance and this keyboard
+
 class Students {
     private int rollno;
     private String Name;
 
-    public void setRollno(int a) {
-        rollno = a;
+    public Students() {
+        // System.out.println(" in Constructor!");
+        rollno = 12;
+        Name = "Jhon";
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
     }
 
     public int getRollno() {
@@ -16,7 +24,10 @@ class Students {
 public class demo {
     public static void main(String[] args) {
         Students obj = new Students();
-        obj.setRollno(31);
+        Students obj1 = new Students();
         System.out.println(obj.getRollno());
+
+        obj.setRollno(31);
+        // System.out.println(obj.getRollno());
     }
 }
