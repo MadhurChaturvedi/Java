182eeaad3d821131d6780a0b94a506fc66a911fd
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-08-16 18:52:38 +0530

    learn multi level inheritance

--- class19/Demo.java
+++ class19/Demo.java
@@ -2,14 +2,16 @@ package class19;
 
 class Demo {
     public static void main(String[] args) {
-        Calc obj = new Calc();
-        AdvCal newCal = new AdvCal();
+        // Calc obj = new Calc();
+        // AdvCal newCal = new AdvCal();
+        VeryAdvCal obj = new VeryAdvCal();
         int r1 = obj.add(5, 5);
         int r2 = obj.add(5, 5);
-        int r3 = newCal.div(10, 2);
-        int r4 = newCal.multi(10, 2);
+        int r3 = obj.div(10, 2);
+        int r4 = obj.multi(10, 2);
+        double r5 = obj.power(10, 2);
         // AdvCal
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
 
     }
 }
---
+++ class19/VeryAdvCal.java
@@ -0,0 +1,7 @@
+package class19;
+
+public class VeryAdvCal extends AdvCal {
+    public double power(int n1, int n2) {
+        return Math.pow(n1, n2);
+    }
+}
